44c7e3fab463e6854bef4f05398449f095b1d6ef
Andrew <andrew@pureaquasprings.com>
2022-03-02 09:30:18 +1100

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks() {
+if(value < 42){
+    return 42-value;
+}else{
+    return value-42;
+}
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+function distanceTravelledInFeet(a,b){
+    if(a < b) {
+        return (b-a) * 264;
+    }else{
+        return (a-b)*264
+    }
+}function calculatesFarePrice(a,b){
+    const distance = distanceTravelledInFeet(a,b);
+
+    if (distance <= 400){
+        return 0;
+        {else if(distance > 400 && distance <= 2000)}
+        return (distance - 400) * 0.02;
+    }
+}
